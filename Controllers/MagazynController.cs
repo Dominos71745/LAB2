b43f75eac66455253fbe09a8c519cdd7556eb533
Dominos71745 <domistu@poczta.fm>
2023-11-03 19:18:00 +0100

    Add project files.

---
+++ Controllers/MagazynController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LAB2.Controllers
+{
+    public class MagazynController : Controller
+    {
+        private static IList<Magazyn> products = new List<Magazyn>()
+        {
+            new Magazyn(){Id = 1, Produkt = "Koszulka" , Cena = 99, Ilosc = 10}
+        };
+
+
+
+        // GET: FilmController
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+        // GET: FilmController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(products.FirstOrDefault(x => x.Id == id));
+        }
+
+        // GET: FilmController/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FilmController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Magazyn product)
+        {
+            product.Id = products.Count + 1;
+            products.Add(product);
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: FilmController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(products.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Magazyn product)
+        {
+            Magazyn product1 = products.FirstOrDefault(x => x.Id == id);
+            product1.Produkt = product.Produkt;
+            product1.Cena = product.Cena;
+            product1.Ilosc = product.Ilosc;
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: FilmController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(products.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Magazyn product)
+        {
+            Magazyn product1 = products.FirstOrDefault(x => x.Id == id);
+            products.Remove(product1);
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Controllers/ZamowieniaController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LAB2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LAB2.Controllers
+{
+    public class ZamowieniaController : Controller
+    {
+        private static IList<Zamowienia> orders = new List<Zamowienia>()
+        {
+                 new Zamowienia(){Id = 1, Produkt = "Koszulka", Data = DateTime.Now, WartoscZamowienia = 99, StatusZamowienia = "W realizacji"}
+        };
+
+
+        // GET: FilmController
+        public ActionResult Index()
+        {
+            return View(orders);
+        }
+
+        // GET: FilmController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(orders.FirstOrDefault(x => x.Id == id));
+        }
+
+        // GET: FilmController/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.StatusyZamowienia = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "W realizacji", Text = "W realizacji" },
+            new SelectListItem { Value = "Odebrany przez klienta", Text = "Odebrany przez klienta" }
+        };
+
+            return View();
+        }
+
+        // POST: FilmController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Zamowienia order)
+        {
+            order.Id = orders.Count + 1;
+            orders.Add(order);
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: FilmController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.StatusyZamowienia = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "W realizacji", Text = "W realizacji" },
+            new SelectListItem { Value = "Odebrany przez klienta", Text = "Odebrany przez klienta" }
+        };
+            return View(orders.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Zamowienia order)
+        {
+            Zamowienia order1 = orders.FirstOrDefault(x => x.Id == id);
+            order1.Produkt = order.Produkt;
+            order1.WartoscZamowienia = order.WartoscZamowienia;
+            order1.StatusZamowienia = order.StatusZamowienia;
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: FilmController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(orders.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Zamowienia order)
+        {
+            Zamowienia order1 = orders.FirstOrDefault(x => x.Id == id);
+            orders.Remove(order1);
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
